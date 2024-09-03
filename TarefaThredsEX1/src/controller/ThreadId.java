669e06e4dd9057a8119c76abf3438946202d30d3
HENRIQUE SIQUEIRA DA SILVA <henrique.silva220@fatec.sp.gov.br>
2024-09-03 15:28:46 -0300

    Add files via upload

---
+++ TarefaThredsEX1/src/controller/ThreadId.java
@@ -0,0 +1,18 @@
+package controller;
+
+public class ThreadId extends Thread {
+
+	public ThreadId() {
+		super();
+	}
+
+	
+	
+	
+	public void run() {
+		int tid = (int) threadId();
+		System.out.println("#" + tid);
+	}
+	
+
+}
---
+++ TarefaThredsEX1/src/view/main.java
@@ -0,0 +1,16 @@
+package view;
+
+import controller.ThreadId;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		
+		ThreadId t = new ThreadId();
+		t.start();
+		
+
+	}
+
+}
